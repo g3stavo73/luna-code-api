ecc3088b8ffb1c8329c0ed4e0cf8b34f3f549df8
Gusta <g3stavo73@gmail.com>
2026-07-04 11:08:30 -0300

    Add lib.rs with public API exports

---
+++ scr/lib.rs
@@ -0,0 +1,10 @@
+
+pub use api::LunaApi;
+
+pub use types::{
+    CommandId, CommandInfo, DocumentId, DocumentInfo,
+    Position, Range, SubscriptionId,
+};
+
+pub use events::{Event, EventKind};
+pub use error::LunaError; 
