27e9c88542d16e38df350329ac1c151ed01311a9
Gusta <g3stavo73@gmail.com>
2026-07-04 11:47:02 -0300

    Update lib.rs

--- scr/lib.rs
+++ scr/lib.rs
@@ -1,18 +1,39 @@
+mod buffer;
+mod document;
 
 pub mod api;
-pub mod buffer;
 pub mod commands;
-pub mod document;
 pub mod errors;
 pub mod events;
 pub mod types;
 
 pub use api::LunaApi;
 
+pub use errors::LunaError;
+pub use events::{Event, EventKind};
+
 pub use types::{
-    CommandId, CommandInfo, DocumentId, DocumentInfo,
-    Position, Range, SubscriptionId,
+    CommandId,
+    CommandInfo,
+    DocumentId,
+    DocumentInfo,
+    Position,
+    Range,
+    SubscriptionId,
 };
 
-pub use events::{Event, EventKind};
-pub use errors::LunaError;
+pub mod prelude {
+    pub use crate::{
+        CommandId,
+        CommandInfo,
+        DocumentId,
+        DocumentInfo,
+        Event,
+        EventKind,
+        LunaApi,
+        LunaError,
+        Position,
+        Range,
+        SubscriptionId,
+    };
+}
